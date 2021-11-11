d0f4ccbb44fee9d6b474b6ca43631644c798c04c
Noah <crashlive112@gmail.com>
2021-11-11 18:47:48 -0500

    commit tree b97197d28432cdba53667fac214c71ea101b61db

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(names, matchCase) {
+  const matchingNames = names.filter(names => names.toUpperCase() === matchCase.toUpperCase());
+  return !!matchingNames ? matchingNames : []
+}
+
+function fuzzyMatch(names, matchCase) {
+  const matchingNames = names.filter(names => names.startsWith(matchCase));
+  return !!matchingNames ? matchingNames : []
+}
+
+const matchName = (driver, matchCase) => {
+  const matchingObjects = driver.filter(driver => driver.name === matchCase);
+  return matchingObjects;
+}
